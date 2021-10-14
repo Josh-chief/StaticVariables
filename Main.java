83370cd6b701ef07d1f034811da447328e62f3f3
chief Dev <69991451+Josh-chief@users.noreply.github.com>
2021-10-14 12:11:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person One = new Person("Joshua",136346);
+        Person Two = new Person("Eric",15555);
+        One.personDetails();
+        Two.personDetails();
+
+
+        //Two.personDetails();
+    }
+}
